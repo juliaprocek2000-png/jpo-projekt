ed0135da3365d7d2b6c6954469938904e9d4de94
Julia Procek <juliaprocek2000@gmail.com>
2026-04-07 20:04:48 +0200

    Dodanie struktury folderów i pliku main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file main.cpp
+ * @brief Glowny plik aplikacji analizujacej dokumentacje przy uzyciu modelu Bielik.
+ * @author Julia
+ */
+
+#include <iostream>
+
+/**
+ * @brief Punkt wejscia do aplikacji.
+ * @return int Status zakonczenia programu.
+ */
+int main() {
+    std::cout << "Analizator Bielik - Projekt JPO gotowy!" << std::endl;
+    return 0;
+}
